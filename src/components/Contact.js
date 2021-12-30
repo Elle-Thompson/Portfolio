c66a4c742b12166d6505e5bd8cee2c78a422dd13
Elle Thompson <elletee@Elles-iMac.attlocal.net>
2021-12-29 21:44:06 -0800

    re-styled contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,24 +3,19 @@ import {Link} from "react-router-dom"
 const Contact = () => {
 
 
-
-	function subForm  ()  {
-
-   
-    fetch("https://api.apispreadsheets.com/data/ho0MbvJimDpFWFGO/", {
-        method: "POST",
-        body: JSON.stringify({"data": {"full_name":"","email":"","message":""}}),
-    }).then(res =>{
-        if (res.status === 201){
-            // SUCCESS
-        }
-        else{
-            // ERROR
-        }
-    })
-	
- }
-
+ const submitForm = () => {
+	fetch("https://api.apispreadsheets.com/data/53IM9m0KDOByMSym/", {
+	method: "POST",
+	body: JSON.stringify({"data": {"email":"","message":"","full_name":""}}),
+}).then(res =>{
+	if (res.status === 201){
+		// SUCCESS
+	}
+	else{
+		// ERROR
+	}
+})
+}
 
 
     return (
@@ -37,17 +32,16 @@ const Contact = () => {
         <div className="div-right"> <h2 id="heading">Contact Me</h2>
         
        
-<form id="contactForm">
-    <label>Full Name</label>
-<input type="text" name="full_name" placeholder="Name"/>
-<label>Email</label>
-<input type="text" name="email" placeholder="Email"/>
-<label>Message</label>
-<textarea type="text" name="message" placeholder="Message here..."/>
-    <button onclick={subForm} id="formButton">Submit</button>
+        <form id="contactForm">
+<input className="name-input" type="text" name="full_name" placeholder="Name"/>
+<input className="email-input" type="text" name="email" placeholder="Email"/>
+<textarea className="message-input" name="message" placeholder="Message here"></textarea>
+<button onClick={submitForm} id="formButton">Submit</button> 
+{/* <button onClick={submitForm} id="formButton">Submit</button> */}
 </form>
 
-        
+
+       
        <div className="contact-links">  
         <a href="https://www.linkedin.com/in/ellethompson01/"> <i class="fab fa-linkedin fa-3x"></i> </a>
        <a href="https://github.com/Elle-Thompson"> <i class="fab fa-github fa-3x"></i></a>
@@ -55,16 +49,10 @@ const Contact = () => {
          </div>
          </div>
 
-       
-
-
-
-
-         
-
         </div>
 
     )
 }
 
 export default Contact;
+
