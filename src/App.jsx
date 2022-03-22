8ef35bea3b50197cbc920b84b3466f9a5e4118c8
Elle Thompson <louis.thompson@post.com>
2022-03-22 15:40:52 -0700

    moved skills-icons css from app.css desktop to Skills.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Projects from './screens/Projects';
 import AboutMe from './screens/AboutMe/AboutMe';
-import Skills from './screens/Skills';
+import Skills from './screens/Skills/Skills';
 import Resume from './screens/Resume';
 import Home from './screens/Home';
 import Contact from './screens/Contact/Contact';
