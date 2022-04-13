5d80a17599feea421e1aee6051eaaf0f17232d93
Elle Thompson <90813454+Elle-Thompson@users.noreply.github.com>
2022-04-13 16:15:20 -0700

    Cleanup (#8)
    * changed node version on package.json
    
    * updated node version in package.json
    
    * small grammar fix to text node deployment
    
    * node changes
    
    * added new screen files

---
+++ src/screens/Resume/Resume.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import DivLeft from '../../components/DivLeft';
+import Footer from "../../components/footer";
+import './Resume.css'
+
+const Resume = () => {
+  const pdf =
+    "https://drive.google.com/file/d/1BRvbmw7olyGPdgKbQnH6_Si8DU8_Otyk/preview";
+
+  return (
+    <div className="wrapper resume-page">
+      <DivLeft />
+
+      <div id="resume" className="div-right">
+        <h2 id="heading">Resume</h2>
+        <iframe title="resume" src={pdf} allow="autoplay" />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Resume;
