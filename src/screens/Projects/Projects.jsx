6c9f7bedba920024e26873e2ec42e6cf188e8144
Elle Thompson <louis.thompson@post.com>
2022-08-16 22:55:30 -0700

    adjusted project images using grid

--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -1,85 +1,85 @@
-import React from 'react'
+import React from "react";
 import Footer from "../../components/footer";
-import './Projects.css'
+import "./Projects.css";
 import Banner from "../../components/Banner-P";
 
 const Projects = () => {
   return (
     <div className="projects-page-wrapper ">
-
       <div className="div-right">
         <div id="header">
-          
           <h2 id="heading">Projects</h2>
 
-          <Banner/>
-          
+          <Banner />
+          <Footer />
         </div>
-        <div id="project-wrapper">
-          <div id="proj1">
-            <a href="https://elle-thompson-metime.netlify.app/">
-              <img
-                id="proj-img"
-                src="https://i.ibb.co/47F28TT/Screen-Shot-2021-12-27-at-7-02-40-PM.png"
-                alt="project1"
-              />
-            </a>
-          </div>
-          <div id="proj1-description">
-            
-            <h4> MeTime</h4>
-            <p>
-              A game inspired by Bop-it and whack-a-mole. Built with react.js,
-              HTML, and javascript, and styled with CSS.
-            </p>
-          </div>
+        <div className="project-wrapper">
+         
+            <div className="proj-img1">
+              <a href="https://elle-thompson.github.io/rick-morty-project2/#/">
+                <img
+                  id="proj-img"
+                  src="https://i.ibb.co/wMRXSgv/Screen-Shot-2021-12-27-at-7-17-18-PM.png"
+                  alt="project1"
+                />
+              </a>
+            </div>
 
-          <div id="proj2">
-            
-            <a href="https://elle-thompson.github.io/rick-morty-project2/#/">
-              <img
-                id="proj-img"
-                src="https://i.ibb.co/wMRXSgv/Screen-Shot-2021-12-27-at-7-17-18-PM.png"
-                alt="project2"
-              />
-            </a>
-          </div>
+            <div className=" proj-img2">
+              <a href="https://elle-thompson-metime.netlify.app/">
+                <img
+                  id="proj-img"
+                  src="https://i.ibb.co/47F28TT/Screen-Shot-2021-12-27-at-7-02-40-PM.png"
+                  alt="project2"
+                />
+              </a>
+            </div>
 
-          <div id="proj2-description">
-            
-            <h4> Rick and Morty</h4>
-            <p>
-              
-              Search engine using Rick and Morty API. Built with react, HTML,
-              and javascript, and styled with CSS.
-            </p>
-          </div>
+            <div className="proj-img3">
+              <a href="https://ga-runtime-terror.github.io/toucan/">
+                <img
+                  id="proj-img"
+                  src="https://i.ibb.co/Th5Qtyk/Screen-Shot-2021-12-27-at-7-20-53-PM.png"
+                  alt="project3"
+                />
+              </a>
+            </div>
 
-          <div id="proj3">
-            <a href="https://ga-runtime-terror.github.io/toucan/">
-              
-              <img
-                id="proj-img"
-                src="https://i.ibb.co/Th5Qtyk/Screen-Shot-2021-12-27-at-7-20-53-PM.png"
-                alt="project3"
-              />
-            </a>
-          </div>
+            <div className="proj-img4"> 
+            <p id="more">  More to come!  </p>
+               </div>
 
-          <div id="proj3-description">
-            {" "}
-            <h4>To-do list</h4>
-            <p>
-              {" "}
-              User auth. Front-end built with react, HTML, and javascript, and
-              styled with CSS. Back-end built with node.js, mongo, and express.
-              Also uses a quote api.
-            </p>{" "}
+            <div className="proj1-des">
+              <h4> Rick and Morty</h4>
+              <p>
+                Search engine using Rick and Morty API. Built with react, HTML,
+                and javascript, and styled with CSS.
+              </p>
+            </div>
+
+            <div className="proj2"></div>
+            <div className="proj2-description">
+              <h4> MeTime</h4>
+              <p>
+                A game inspired by Bop-it and whack-a-mole. Built with react.js,
+                HTML, and javascript, and styled with CSS.
+              </p>
+            </div>
+
+            <div className="proj3"></div>
+
+            <div className="proj3-description">
+              <h4>To-do list</h4>
+              <p>
+                User auth. Front-end built with react, HTML, and javascript, and
+                styled with CSS. Back-end built with node.js, mongo, and
+                express. Also uses a quote api.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <Footer />
-    </div>
+    
   );
 };
 
