0dac588f04348415625fa4672845fd7b60fb03b6
Elle Thompson <louis.thompson@post.com>
2022-08-20 13:47:26 -0700

    added NavBar to resume page and removed divleft

--- src/screens/Resume/Resume.jsx
+++ src/screens/Resume/Resume.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import DivLeft from '../../components/DivLeft';
 import Footer from "../../components/footer";
 import './Resume.css'
+import NavBar from "../../components/NavBar";
+
 
 const Resume = () => {
+
+  
   const pdf = "https://drive.google.com/file/d/1sQAdTOr3RErwvkT8Oxu64W7H6Xw9WT2U/preview";
 
 
   return (
     <div className="wrapper resume-page">
-      <DivLeft />
+
+<NavBar/>
+
 
       <div id="resume" className="div-right">
         <h2 id="heading">Resume</h2>
