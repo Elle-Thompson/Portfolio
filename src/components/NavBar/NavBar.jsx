6405b1da069ca2c14c2454694a7ac088212c7ffa
Elle Thompson <louis.thompson@post.com>
2022-08-31 15:23:04 -0700

    created functional hamburger menu in nav component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,52 +12,49 @@ const NavBar = () => {
     });
   });
 
-  
-  
-
   return (
     <nav id="NavBar">
-      <div className="container menu-items">
-      
-      <ul className="link-list">
-        <li className="link-item">
-          <a>
-            <Link to="/">Home</Link>
-          </a>
-        </li>
+      <div className=" container nav-container">
+        <input className="checkbox" type="checkbox" name="" id="checkbox" />
+        <div className="hamburger">
+          <span className="bar line1"> </span>
+          <span className="bar line2"> </span>
+          <span className="bar line3"> </span>
+        </div>
 
-        <li className="link-item">
-          <a>
-            <Link to="/AboutMe">About Me</Link>
-          </a>
-        </li>
+        <div className="menu-items">
+          <ul className="link-list">
+            <li className="link-item">
+              <a>
+                <Link to="/">Home</Link>
+              </a>
+            </li>
 
-        <li className="link-item">
-          <a>
-            <Link to="/Projects">Projects</Link>
-          </a>
-        </li>
+            <li className="link-item">
+              <a>
+                <Link to="/AboutMe">About Me</Link>
+              </a>
+            </li>
 
-        <li className="link-item">
-          <Link to="/Skills">Tech Stack</Link>
-        </li>
+            <li className="link-item">
+              <a>
+                <Link to="/Projects">Projects</Link>
+              </a>
+            </li>
 
-        <li className="link-item">
-          <Link to="/Resume">Resume</Link>
-        </li>
+            <li className="link-item">
+              <Link to="/Skills">Tech Stack</Link>
+            </li>
 
-        <li className="link-item">
-          <Link to="/Contact">Contact</Link>
-        </li>
-      </ul>
-</div> 
-<div className="nav-container">
-<input className="checkbox" type="checkbox" name="" id="" />
-      <div className="hamburger">
-        <span className="bar line1"> </span>
-        <span className="bar line2"> </span>
-        <span className="bar line3"> </span>
-      </div> 
+            <li className="link-item">
+              <Link to="/Resume">Resume</Link>
+            </li>
+
+            <li className="link-item">
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
